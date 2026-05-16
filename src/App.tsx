5c79cce26ebf9afb1253e0274e3a263f8758235e
아루 <subil0306ab@gmail.com>
2026-05-16 16:18:07 +0900

    feat:  App에서 useEffect로 API 호출하기

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,23 @@ import Header from "./components/Header/Header";
 import MovieList from "./components/MovieList/MovieList";
 import { sampleMovies } from "./mocks/movies";
 import "./App.css";
-import { useState } from "react";
-interface Movie {
-  id: number;
-  title: string;
-}
+import { useState, useEffect } from "react";
+import type { Movie } from "./types/movie";
+import { fetchPopularMovies } from "./apis/movieApi";
 
 function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
   // TODO 5. 영화 목록을 저장할 state를 만드세요.
   // 힌트: useState를 import한 뒤 아래 형태로 작성합니다.
   // const [movies, setMovies] = useState<Movie[]>([])
+  useEffect(() => {
+    async function loadMovies() {
+      const movies = await fetchPopularMovies();
+      setMovies(movies);
+    }
 
+    loadMovies();
+  }, []);
   // TODO 6. 컴포넌트가 처음 렌더링될 때 인기 영화 목록을 불러오세요.
   // 힌트: useEffect를 import한 뒤 fetchPopularMovies()를 호출합니다.
   // 힌트: 가져온 영화 목록은 setMovies로 저장합니다.
