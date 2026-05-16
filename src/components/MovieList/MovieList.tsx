def461c1950e914ec7e995c8a4378ef92445816c
아루 <subil0306ab@gmail.com>
2026-05-16 14:32:54 +0900

    feat: MovieList에서 배열 반복 렌더링
    *feat: 각 영화 데이터마다 MoiveCard 컴포넌트를 렌더링하게 한다.

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -1,8 +1,9 @@
-import type { Movie } from '../../types/movie'
-import './MovieList.css'
+import type { Movie } from "../../types/movie";
+import MovieCard from "../MovieCard/MovieCard";
+import "./MovieList.css";
 
 interface MovieListProps {
-  movies: Movie[]
+  movies: Movie[];
 }
 
 function MovieList({ movies }: MovieListProps) {
@@ -11,13 +12,16 @@ function MovieList({ movies }: MovieListProps) {
       <h2 className="movie-list-title">지금 인기있는 영화</h2>
 
       <div className="movies" data-movie-count={movies.length}>
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
         {/* TODO 1. movies 배열을 map으로 반복 렌더링하세요. */}
         {/* 힌트: MovieCard 컴포넌트를 사용하세요. */}
         {/* 힌트: MovieCard를 import해야 합니다. */}
         {/* 힌트: key에는 movie.id를 넣으세요. */}
       </div>
     </section>
-  )
+  );
 }
 
-export default MovieList
+export default MovieList;
